8f1404095e70a872715d33126cd0b2d2c94ab527
Guedesmavi <mariavitoriaguedesferreira@gmail.com>
2026-05-25 08:44:40 -0300

    post model

--- src/models/respostaModel.js
+++ src/models/respostaModel.js
@@ -16,3 +16,20 @@ async function buscarPorId(id) {
   );
   return result.rows[0];
 }
+
+async function criar(dados) {
+  const { resp_correta, explicacao_prof, contas_url, videoaula } = dados;
+
+  const sql = `
+    INSERT INTO resposta (resp_correta, explicacao_prof, contas_url, videoaula)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+  `;
+  
+  const result = await pool.query(
+    sql,
+    [resp_correta, explicacao_prof, contas_url, videoaula]
+  );
+  
+  return result.rows[0];
+}
