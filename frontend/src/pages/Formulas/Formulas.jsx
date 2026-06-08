6204eaa083a7a00dd44543133d444b0dca110184
gabriellacstavarengo <gabriellacstavarengo@gmail.com>
2026-06-08 10:34:42 -0300

    second

--- frontend/src/pages/Formulas/Formulas.jsx
+++ frontend/src/pages/Formulas/Formulas.jsx
@@ -17,3 +17,27 @@ export default function Formulas() {
         </p>
       </div>
 
+  <div className={styles.container}>
+        <div className={styles.card}>
+          <h2>MECÂNICA</h2>
+          <div className={styles.formula}>
+            <span>VELOCIDADE MÉDIA</span>
+            <div className={styles.caixa}>
+              Vm = ΔS / Δt
+            </div>
+          </div>
+
+          <div className={styles.formula}>
+            <span>MRU</span>
+            <div className={styles.caixa}>
+              S = S₀ + V·T
+            </div>
+          </div>
+
+          <div className={styles.formula}>
+            <span>TORRICELLI</span>
+            <div className={styles.caixa}>
+              v² = v₀² + 2·a·Δs
+            </div>
+          </div>
+        </div>
